a4962dcf10c11fd4d2645bc48e30e3dc3cac316b
Krusovice <jmk1589@gmail.com>
2026-01-23 22:43:23 +0100

    splitting the ai monitor script template to a proper functions, so it can be adapted to suit the purpose

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
+/* 
 use axum::{
     extract::ws::{Message, WebSocket, WebSocketUpgrade},
     response::IntoResponse,
     routing::get,
     Router,
 };
+*/
+use serde_json::Value;
 use sysinfo::System;
-use std::time::Duration;
-use std::net::SocketAddr;
+// use std::time::Duration;
+// use std::net::SocketAddr;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::collections::VecDeque;
 
+
+fn main() {
+    println!("Server Monitoring Initializing!");
+    let sys = System::new();
+
+    let metrics = read_metrics(sys);
+    println!("{}", metrics);
+}
+
+fn read_metrics( mut sys: System ) -> Value {
+    sys.refresh_cpu_all();
+    sys.refresh_memory();
+
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
+
+    let collected_data = serde_json::json!({
+        "cpu": sys.global_cpu_usage(),
+        "ram": (sys.used_memory() as f64 / sys.total_memory() as f64),
+        "timestamp": timestamp
+        });
+
+    collected_data
+}
+/*
 
 #[tokio::main]
 async fn main() {
@@ -30,43 +60,36 @@ async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
     ws.on_upgrade(handle_socket)
 }
 
-async fn handle_socket(mut socket: WebSocket) {
-    println!("Server Monitoring Initializing!");
-    let mut sys = System::new();
-    let mut data: VecDeque<serde_json::Value> = VecDeque::with_capacity(3600);
 
-    loop {
-        sys.refresh_cpu_all();
-        sys.refresh_memory();
-
-        let timestamp = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_secs();
-
-        let collected_data = serde_json::json!({
-            "cpu": sys.global_cpu_usage(),
-            "ram": (sys.used_memory() as f64 / sys.total_memory() as f64),
-            "timestamp": timestamp
-            });
-
-        // remove oldest data
-        data.push_back(collected_data);
-
-        if data.len() == 3600 {
-            data.pop_front(); 
-        }
-
-        let json_string = serde_json::to_string(&data).unwrap();
-
-        if socket
-            .send(Message::Text(json_string.into()))
-            .await
-            .is_err()
-        {
-            break;
-        }
-
-        tokio::time::sleep(Duration::from_secs(1)).await;
+handle_socket(mut socket: WebSocket) {
+    if socket
+        .send(Message::Text(json_string.into()))
+        .await
+        .is_err()
+    {
+        break;
     }
 }
+
+let mut data: VecDeque<serde_json::Value> = VecDeque::with_capacity(3600);
+
+fn store_metrics_data(
+    mut data_array: VecDeque<serde_json::Value>,
+    collected_data: serde_json::json! ) {
+
+    // remove oldest data
+    data_array.push_back(collected_data);
+
+    if data_array.len() == 3600 {
+        data_array.pop_front(); 
+    }
+
+    let json_string = serde_json::to_string(&data).unwrap();
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+}
+
+
+
+*/
+
